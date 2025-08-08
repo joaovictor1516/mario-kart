240f8c40467bc1fe1d15edeebf31f1ad40ecde31
joaovictor1516 <voliveira.joao30@sempreceub.com>
2025-08-08 14:39:33 -0300

    alteracao no arquivo src/rolagemDados.js, criacao da funcao rolagemDadosPista, que e responsavel pela selecao da pista.

--- src/rolagemDados.js
+++ src/rolagemDados.js
@@ -1,9 +1,19 @@
-exports.rolagemDado = () => {
+exports.rolagemDadoAtrinuto = () => {
     const rolagem = 1 + Math.floor(Math.random() * 10);
 
     if(rolagem <= 6){
         return rolagem;
     } else{
         return rolagem - 6;
+    }
+}
+
+exports.rolagemDadoPista = () => {
+    const rolagem = 1 + Math.floor(Math.random() * 10);
+
+    if(rolagem <= 3){
+        return rolagem;
+    } else{
+        return rolagem - 3;
     }
 }
